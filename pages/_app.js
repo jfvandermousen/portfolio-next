4657a1291d69a6588a8a94355eb97805e23b5da0
jfvandermousen <jeanfrancoisvandermousen@gmail.com>
2021-10-06 11:53:48 +0200

    configure email.js and react alert

--- pages/_app.js
+++ pages/_app.js
@@ -4,13 +4,23 @@
   import Background from '../components/Background';
   import Footer from '../components/Footer';
   import Fade from 'react-reveal/Fade';
+  import { transitions, positions, Provider as AlertProvider } from 'react-alert'
+  import AlertTemplate from 'react-alert-template-basic'
 
-
+  const options = {
+    // you can also just use 'bottom center'
+    position: positions.BOTTOM_CENTER,
+    timeout: 5000,
+    offset: '30px',
+    // you can also just use 'scale'
+    transition: transitions.FADE
+  }
 
 
   function MyApp({ Component, pageProps }) {
     return (
       <>
+      <AlertProvider template={AlertTemplate} {...options}>
       <Background className="bg"/>
       <div className="container mx-auto px-8 pt-8">
             
@@ -20,6 +30,7 @@
             <Fade bottom delay={11500}>
             <Footer/>
             </Fade>
+            </AlertProvider>
             </>
           )
   }
