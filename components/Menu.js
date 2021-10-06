61aa5a7193698ed1d5e298535014caf1ee98230a
jfvandermousen <jeanfrancoisvandermousen@gmail.com>
2021-10-06 12:16:13 +0200

    fixed burger menu

--- components/Menu.js
+++ components/Menu.js
@@ -7,13 +7,15 @@ import Link from "next/link"
 const Menu = () => {
     const [showMenu,setShowMenu] = useState(false);
     // const [isOpen, setOpen] = useState(false)
+
+    
     let menu
     
 
     if(showMenu){
         menu =  
         <Fade delay={1000} >
-        <div  onClick={()=>setShowMenu(!showMenu)}className="fixed bg-black h-full w-full top-0 left-0 z-50 flex flex-col justify-center items-center" >
+        <div className="fixed bg-black h-full w-full top-0 left-0 z-50 flex flex-col justify-center items-center" >
          <Link href="/"><div className="p-4" onClick={()=>setShowMenu(!showMenu)}> <p className="nav-overlay text-4xl">Home</p></div></Link>
             <Link href="/Projects" ><div className="p-4" onClick={()=>setShowMenu(!showMenu)}><p className="nav-overlay text-4xl ">Projects</p></div></Link>
             <Link href="/Contact" ><div className="p-4" onClick={()=>setShowMenu(!showMenu)}><p className=" nav-overlay text-4xl ">Contact</p></div></Link>
@@ -25,9 +27,9 @@ const Menu = () => {
     return (
         <div className="above">
         <div className=" navBar flex flex-row justify-end" >
-            <Hamburger  duration={1}  onToggle={()=>setShowMenu(!showMenu) }  />
+            <Hamburger  duration={1}  toggled={showMenu} toggle={setShowMenu}  />
         </div>
-        {menu}
+        {menu }
         </div>
 
     )
